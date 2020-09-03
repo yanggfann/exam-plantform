fcefff300397cfb294c889e70f3051c4310992c8
yanggfann <fan.yang1@thoughtworks.com>
2020-09-03 23:37:39 +0800

    examination context controller add test to validate api return value

--- src/test/java/exam/examinationContext/userInterface/ExaminationControllerTest.java
+++ src/test/java/exam/examinationContext/userInterface/ExaminationControllerTest.java
@@ -6,28 +6,16 @@ import exam.examinationContext.application.CreateExaminationCommand.Paper;
 import exam.examinationContext.application.CreateExaminationCommand.Paper.BlankQuiz;
 import exam.examinationContext.application.ExaminationApplicationService;
 import exam.examinationContext.domain.model.examination.ExaminationId;
-import org.assertj.core.util.Lists;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -60,7 +48,7 @@ class ExaminationControllerTest {
         String requestJson = JSONObject.toJSONString(createExaminationCommand);
 
         //when
-        when(examinationApplicationService.createExamination(createExaminationCommand)).thenReturn(examinationId);
+        when(examinationApplicationService.createExamination(any())).thenReturn(examinationId);
 
         //then
         MvcResult mvcResult = mockMvc
@@ -69,6 +57,7 @@ class ExaminationControllerTest {
                         .content(requestJson))
                 .andExpect(status().is(201)).andReturn();
         String contentAsString = mvcResult.getResponse().getContentAsString();
+        assertEquals(contentAsString, "{\"uri\":\"examinations/000\"}");
 
         verify(examinationApplicationService, times(1)).createExamination(any());
     }
